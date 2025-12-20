eb051e9cff69b3d135ec3c9e90aae01ac7a1abbb
yeguchocku <yegammai2006@gmail.com>
2025-12-20 11:19:32 +0530

    Fix Render crash

--- backend/src/index.js
+++ backend/src/index.js
@@ -31,8 +31,8 @@ app.use("/api/messages",messageRoutes);
 if(process.env.NODE_ENV =="production" ){
   app.use(express.static(path.join(__dirname,"../frontend/dist")));
 
-  app.get("/*",(req,res)=>{
-    res.sendFile(path.join(__dirname,"../frontend","dist","index.html"));
+  app.get(/.*/,(req,res)=>{
+    res.sendFile(path.join(__dirname,"../frontend/dist/index.html"));
   })
 }
 /* ✅ START SERVER + DB */
