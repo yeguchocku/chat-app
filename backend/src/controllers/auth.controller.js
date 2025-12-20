97e0f59f02a2daa1d70891125864348351bae8bb
yeguchocku <yegammai2006@gmail.com>
2025-12-20 19:45:48 +0530

    14th attempt

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,4 +1,4 @@
-import { generateToken } from "../lib/utils.js";
+import { generateToken } from "../lib/utlis.js";
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
 import cloudinary from "../lib/cloudinary.js";
