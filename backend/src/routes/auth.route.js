02c8ca7358c25c65edf092010d5b1d654718c35c
yeguchocku <yegammai2006@gmail.com>
2025-12-20 19:41:24 +0530

    13th attempt

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { checkAuth, login, logout, signup, updateProfile } from "../controllers/auth.controller.js";
-import { checkAuth, login, logout, signup, updateProfile } from "../controllers/auth.controller.js";
+;
 import { protectRoute } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
