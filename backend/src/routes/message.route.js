f14489f80ad9036ac62e0d341754317cf05fbe1a
yeguchocku <yegammai2006@gmail.com>
2025-12-20 12:07:35 +0530

    Fix

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { protectRoute } from "../middleware/auth.middleware.js";
-import { getMessages,getUsersForSidebar,sendMessage } from "../controllers/message.controller.js";
+import { createConversation,getMessages,getUsersForSidebar,sendMessage } from "../controllers/message.controller.js";
 
 const router = express.Router();
 
